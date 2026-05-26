c6cd5e40dc52481f838c5c1dcc26a20fbfce66e1
Himanshi0827 <157987622+Himanshi0827@users.noreply.github.com>
2026-05-26 18:13:22 +0530

    Refactor EditAgreementGroupForm component layout

--- src/EditForms/EditAgreementGroupForm.tsx
+++ src/EditForms/EditAgreementGroupForm.tsx
@@ -1,39 +1,129 @@
 
+// function EditAgreementGroupForm({ data,onChange,onComplete }) {
+ 
+//   return (
+//     <div className="form-card">
+//       <table className="form-table">
+//         <tbody>
+ 
+//           <tr>
+//             <td className="label">AgreementLineItem Name:</td>
+//             <td>
+//               <input name="AgreementLineItemName"
+//                       value={data?.Name || ''}
+//                       style={{cursor:'not-allowed'}}
+//                       type="text"
+//                       disabled
+//                        />
+//             </td>
+//           </tr>
+//           <tr>
+//   <td className="label">Agreement Group</td>
+//   <td>
+//     <input name="agreementGroup"
+//           value={data?.APTS_Agreement_Group_c?.Name||''}
+//           type="text"
+//           style={{cursor:'not-allowed'}}
+//           disabled
+//     />
+//   </td>
+// </tr>
+ 
+//         </tbody>
+//       </table>
+ 
+//     </div>
+//   );
+// }
+ 
+// export default EditAgreementGroupForm;
+
 function EditAgreementGroupForm({ data,onChange,onComplete }) {
  
   return (
-    <div className="form-card">
-      <table className="form-table">
-        <tbody>
+//     <div className="form-card">
+//       <table className="form-table">
+//         <tbody>
  
+//           <tr>
+//             <td className="label">AgreementLineItem Name:</td>
+//             <td>
+//               <input name="AgreementLineItemName"
+//                       value={data?.Name || ''}
+//                       style={{cursor:'not-allowed'}}
+//                       type="text"
+//                       disabled
+//                        />
+//             </td>
+//           </tr>
+//           <tr>
+//   <td className="label">Agreement Group</td>
+//   <td>
+//     <input name="agreementGroup"
+//           value={data?.APTS_Agreement_Group_c?.Name||''}
+//           type="text"
+//           style={{cursor:'not-allowed'}}
+//           disabled
+//     />
+//   </td>
+// </tr>
+ 
+//         </tbody>
+//       </table>
+ 
+//     </div>
+<div>
+  <div className="proposal-table-container">
+    <table className="proposal-table">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Commercial Conditions</th>
+          <th>Group Name</th>
+          <th>Timeperiod for member tier change review</th>
+          <th>ALI Name</th>
+        </tr>
+      </thead>
+ 
+      <tbody>
           <tr>
-            <td className="label">AgreementLineItem Name:</td>
+            {/* radio */}
             <td>
-              <input name="AgreementLineItemName"
-                      value={data?.Name || ''}
-                      style={{cursor:'not-allowed'}}
-                      type="text"
-                      disabled
-                       />
+              <input
+                type="radio"
+                name="agreementGroup"
+                checked={true} />
             </td>
+ 
+            {/* icon/button */}
+            <td style={{'alignItems':'center'}}>
+              <button>
+                <b>⊝</b>
+              </button>
+            </td>
+ 
+            {/* group */}
+            <td><a
+      href={`https://preview-rls09.congacloud.com/admin/entity/APTS_Agreement_Groups_c/detail/${data.APTS_Agreement_Group_c?.Id}/`}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="clone-link"
+    > {data.APTS_Agreement_Group_c?.Name}
+    </a></td>
+ 
+            {/* review */}
+            <td>
+              {
+                "Annually"}
+            </td>
+            <td>{data?.Name}</td>
           </tr>
-          <tr>
-  <td className="label">Agreement Group</td>
-  <td>
-    <input name="agreementGroup"
-          value={data?.APTS_Agreement_Group_c?.Name||''}
-          type="text"
-          style={{cursor:'not-allowed'}}
-          disabled
-    />
-  </td>
-</tr>
- 
-        </tbody>
-      </table>
- 
-    </div>
+      </tbody>
+    </table>
+  </div>
+</div>
   );
 }
  
 export default EditAgreementGroupForm;
+
